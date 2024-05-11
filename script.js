aa888e6eb43056c978e2c336401729d7046da25a
Ademola Elisha <ademolaelishaoluwapelumi@gmail.com>
2024-05-11 11:45:58 +0100

    header3

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let menu = document.querySelector('#menu-bar')
 let navbar = document.querySelector('.navbar')
+let header = document.querySelector('.header-3')
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times')
@@ -9,4 +10,13 @@ menu.addEventListener('click', () => {
 window.onscroll = () => {
     menu.classList.remove('fa-times')
     navbar.classList.remove('active')
+
+    if (window.scrollY > 250) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
 }
+
+
+
